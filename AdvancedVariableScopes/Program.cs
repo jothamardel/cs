ba4c4695ce1e2cb07a648674180f381635c0947a
jothamardel <ardelmbiplang@gmail.com>
2023-08-17 13:38:19 +0100

    advanced variable scope, variables available to functions and classess
    Signed-off-by: jothamardel <ardelmbiplang@gmail.com>

--- AdvancedVariableScopes/Program.cs
+++ AdvancedVariableScopes/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Book book1 = new Book("Harry Potter and the Sorcerer's Stone.", "J.K. Rowlings");
 
+book1.Printsummary();
+Console.WriteLine(book1.GetTitleLengthWithoutWhitespace());
+
+
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public void Printsummary()
+    {
+        string summary = $"{Title} - {Author}";
+        Console.WriteLine(summary);
+    }
+
+    public int GetTitleLengthWithoutWhitespace()
+    {
+        string titleWithoutWhitespaces = Title.Replace(" ", "");
+        return titleWithoutWhitespaces.Length; 
+    }
+}
